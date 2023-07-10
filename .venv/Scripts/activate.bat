11dbfd58cd1f70adbeb819d65c3d444fbdae8165
Illia Duverkher Natiahin/Smart TV Apps (SWQA) /SRPOL/Professional/Samsung Electronics <i.duverkher@samsung.com>
2023-07-10 11:28:15 +0200

    adding gitignore

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\i.duverkher\Desktop\important\python\py-RestApi\.venv
+set VIRTUAL_ENV=C:\Users\i.duverkher\Desktop\important\py-RestApi\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
