0e36293a0dfcf9191860413ecdec5939872009b3
Illia Duverkher Natiahin/Smart TV Apps (SWQA) /SRPOL/Professional/Samsung Electronics <i.duverkher@samsung.com>
2023-07-13 08:51:38 +0200

    adding improvments

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10
 EXPOSE 5000
 WORKDIR /app
 COPY requirement.txt .
-RUN pip install -r requirement.txt
+RUN python -m pip install -r requirement.txt
 COPY . .
 CMD ["flask", "run" , "--host", "0.0.0.0"] 
 #docker build -t rest-apis-flask-python .
--- confluenceRestApi/confluence_RestAPI.py
+++ confluenceRestApi/confluence_RestAPI.py
@@ -1,12 +1,16 @@
 # This code sample uses the Confluence RestAPI:
 # Created by Illia Duverkher-Natiahin
 
+import re
 import requests
 from requests.auth import HTTPBasicAuth
+import urllib3
 import json
 
+urllib3.disable_warnings()
+
 url = "https://kb.sprc.samsung.pl/rest/api/content/"
-auth = HTTPBasicAuth("<API_USER>", "<API_TOKEN>")
+auth = HTTPBasicAuth ("i.duverkher", "4i8c66j5q2o344ercc7kmlrq0u9mofq")
 
 
 def get_page_json(page_id, expand=False):
@@ -37,11 +41,29 @@ def get_page_json(page_id, expand=False):
     return json_data  # ['body']['storage']['value']
 
 
-print(get_page_json("563617140", 'body.storage'))  # Iza page_id is 560881462
+#print(get_page_json("563617140", 'body.storage'))  # Iza page_id is 560881462
 
 
 def get_and_set(page_id):
     json_data = get_page_json(page_id)
+
+    fname = "myconfluence.html" # this is the name of my html file
+    html_file = open(fname, 'r', encoding='utf-8')
+    html_source_code = html_file.read()  
+    print(html_source_code)
+
+    macro_content = f'''
+                    <!-- BEGIN HTML Macro -->
+                    <ac:structured-macro ac:name="html">
+                        <ac:plain-text-body>
+                            <![CDATA[
+                                {html_source_code}
+                            ]]>
+                        </ac:plain-text-body>
+                    </ac:structured-macro>
+                    <!-- END HTML Macro -->
+                    '''
+                   
 
     new_json_data = dict()
     new_json_data['id'] = json_data['id']
@@ -55,8 +77,8 @@ def get_and_set(page_id):
         new_json_data['key'] = json_data['key']
 
     new_json_data['body'] = {'storage': {
-        'value': '<p>the info you see here was created with Python using confluence Rest-API</p>' +
-        '<p><table><tr><th>head1</th><th>head2</th></tr><tr><td>value1</td><td>value2</td></tr><tr><td>value3</td><td>value4</td></tr></table></p>', 'representation': 'storage'}}
+                            'value': macro_content , 
+                            'representation': 'storage'}}
     print(set_page_json(page_id, new_json_data))
 
 
@@ -76,7 +98,10 @@ def set_page_json(page_id, json_content):
         verify=False
     )
 
-    return (response.text)
+    if response.status_code == 200:
+        print('HTML macro created successfully.')
+    else:
+        print('Failed to create HTML macro. Error:', response.text)
 
 
 print(get_and_set('563617140'))  # 563617140 confluence testing page
